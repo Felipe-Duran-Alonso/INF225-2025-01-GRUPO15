ca8e1fbeaacab6329b23123a82529bce198281b9
Felipe-Duran-Alonso <felipe.durana@sansano.usm.cl>
2025-06-16 23:09:12 -0400

    Correccion nombres 2

--- djangoproject/myapp/templates_names.py
+++ djangoproject/myapp/templates_names.py
@@ -1,35 +1,29 @@
 #Se agrupan segun tipo 
-DIR_FUENTES = ''
+DIR_FUENTES = 'fuentes/'
 ADMINISTRAR_FUENTE = DIR_FUENTES+'administrar_fuentes.html'
 EDITAR_RESUMEN = DIR_FUENTES+'editar_resumen.html'
 CREAR_RESUMEN_OLD = DIR_FUENTES+'resumir_texto.html'
 CREAR_RESUMEN = DIR_FUENTES+'resumir_texto1.html'
 REVISAR_RESUMEN = DIR_FUENTES+'revisar_resumen.html'
 
-DIR_BOLETINES = ''
+DIR_BOLETINES = 'boletines/'
 EDITAR_BOLETIN_AUX = DIR_BOLETINES+'editar_boletines_aux.html'
 EDITAR_BOLETIN = DIR_BOLETINES+'editar_boletines.html'
 ESTADISTICA_BOLETIN = DIR_BOLETINES+'estadisticas_boletines.html'
 SUBIR_BOLETIN = DIR_BOLETINES+'subir_boletines.html'
 VER_BOLETIN = DIR_BOLETINES+'ver_boletines.html'
 
-DIR_REQUERIMIENTOS = ''
+DIR_REQUERIMIENTOS = 'requerimientos/'
 EDITAR_REQUERIMIENTO_AUX = DIR_REQUERIMIENTOS+'editar_requerimientos_aux.html'
 EDITAR_REQUERIMIENTO = DIR_REQUERIMIENTOS+'editar_requerimientos.html'
 CREAR_REQUERIMIENTO = DIR_REQUERIMIENTOS+'insertar_requerimientos.html'
 REQUERIMIENTO = DIR_REQUERIMIENTOS+'requerimientos.html'
 VER_REQUERIMIENTO = DIR_REQUERIMIENTOS+'ver_requerimientos.html'
 
-DIR_GENERAL = ''
+DIR_GENERAL = 'general/'
 HOME = DIR_GENERAL+'home.html'
 INDEX = DIR_GENERAL+'index.html'
-MENU = DIR_GENERAL+'menu.html'
 CONFIGURACIONES = DIR_GENERAL+'modificar_configuraciones.html'
 REGISTRO = DIR_GENERAL+'registro.html'
 
-
-
-
-
-
-
+MENU = 'menu.html' #No se hace con este template ya que se importa en otros por lo que se DEBE dejar fijo
